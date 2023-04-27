75f040514310242768794856d6aba8b7c8637c13
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-04-27 22:00:46 +0300

    Create ORDERBY.sql

---
+++ ORDERBY.sql
@@ -0,0 +1,5 @@
+SELECT name, year, imdb_rating
+FROM movies
+ORDER BY imdb_rating DESC;
+
+//returns name, year, and imdb_rating columns of all the movies, ordered highest to lowest by their ratings
